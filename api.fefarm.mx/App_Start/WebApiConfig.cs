8963a0da05ba6877519552b734e37436b201f2c8
jfescobar18 <jfescobar18@hotmail.com>
2019-08-28 23:33:35 -0500

    Cors added

--- api.fefarm.mx/App_Start/WebApiConfig.cs
+++ api.fefarm.mx/App_Start/WebApiConfig.cs
@@ -10,6 +10,7 @@ namespace api.fefarm.mx
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
